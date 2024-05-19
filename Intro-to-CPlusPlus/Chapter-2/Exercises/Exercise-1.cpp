11de448bf726ebafd884c51790411dea834f1b35
d3wyth30utl4w <jordandeuley@gmail.com>
2024-05-18 19:23:22 -0500

    Completed exercise 1.

---
+++ Intro-to-CPlusPlus/Chapter-2/Exercises/Exercise-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+
+int main() {
+
+    std::cout << "*********************************" << std::endl;
+    std::cout << "*" << "\t" << "Programming Assignment 1" << "\t" << "*" << std::endl;
+    std::cout << "*" << "\t" << "Computer Programming I" << "\t\t" << "*" << std::endl;
+    std::cout << "*" << "\t" << "Author: Jordan Deuley" << "\t\t" << "*" << std::endl;
+    std::cout << "*" << "\t" << "Due Date: Thursday, Jan. 24" << "\t" << "*" << std::endl;
+    std::cout << "*********************************" << std::endl;
+
+}
